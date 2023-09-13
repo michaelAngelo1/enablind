bf3a3b329dde7ad353ccfe2b36bb4697046244fc
John L <john.lukito@gmail.com>
2023-09-13 12:01:18 +0700

    fix: fixed async loading of details page

--- lib/test/corporate/test_corporate_landing_page.dart
+++ lib/test/corporate/test_corporate_landing_page.dart
@@ -137,12 +137,18 @@ class JobDetailsPage extends StatefulWidget {
 
 class JobDetailsPageState extends State<JobDetailsPage> {
   late Future<DocumentSnapshot<Map<String, dynamic>>> companyData;
+  late bool isLoading = true; // Add a loading state
 
   @override
   void initState() {
     super.initState();
     final companyUid = widget.jobListing['jobCompany'];
-    companyData = FirebaseFirestore.instance.collection('Users/Role/Corporations').doc(companyUid).get();
+    companyData = FirebaseFirestore.instance.collection('Users/Role/Corporations').doc(companyUid).get().then((value) {
+      setState(() {
+        isLoading = false; // Set loading to false when data is received
+      });
+      return value;
+    });
   }
 
   @override
@@ -151,47 +157,51 @@ class JobDetailsPageState extends State<JobDetailsPage> {
       appBar: AppBar(
         title: const Text('Job Details'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
-              future: companyData,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const CircularProgressIndicator();
-                }
-                if (snapshot.hasError || !snapshot.hasData || !snapshot.data!.exists) {
-                  // Handle the case where company data is not available
-                  return const Text('Company data not available');
-                }
-                final companyData = snapshot.data!.data() as Map<String, dynamic>;
-                final companyLogoUrl = companyData['logoUrl'];
-                final companyName = companyData['companyName'];
+      body: isLoading
+          ? const Center(
+              child: CircularProgressIndicator(), // Display spinner while loading
+            )
+          : Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+                    future: companyData,
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const CircularProgressIndicator();
+                      }
+                      if (snapshot.hasError || !snapshot.hasData || !snapshot.data!.exists) {
+                        // Handle the case where company data is not available
+                        return const Text('Company data not available');
+                      }
+                      final companyData = snapshot.data!.data() as Map<String, dynamic>;
+                      final companyLogoUrl = companyData['logoUrl'];
+                      final companyName = companyData['companyName'];
 
-                return Column(
-                  children: [
-                    Image.network(companyLogoUrl),
-                    Text('Company Name: $companyName'),
-                  ],
-                );
-              },
-            ),
-            Text('Job Title: ${widget.jobListing['jobTitle']}'),
-            Text('Job Description: ${widget.jobListing['jobDescription']}'),
-            const Text('Qualifications:'),
-            for (final qualification in (widget.jobListing['jobQualifications'] as List<dynamic>))
-              Padding(
-                padding: const EdgeInsets.only(left: 16.0),
-                child: Text('- $qualification'),
+                      return Column(
+                        children: [
+                          Image.network(companyLogoUrl),
+                          Text('Company Name: $companyName'),
+                        ],
+                      );
+                    },
+                  ),
+                  Text('Job Title: ${widget.jobListing['jobTitle']}'),
+                  Text('Job Description: ${widget.jobListing['jobDescription']}'),
+                  const Text('Qualifications:'),
+                  for (final qualification in (widget.jobListing['jobQualifications'] as List<dynamic>))
+                    Padding(
+                      padding: const EdgeInsets.only(left: 16.0),
+                      child: Text('- $qualification'),
+                    ),
+                  Text('Job Type: ${widget.jobListing['jobType']}'),
+                  Text('Salary: Rp${widget.jobListing['jobSalary']}'),
+                  // Add more job details here
+                ],
               ),
-            Text('Job Type: ${widget.jobListing['jobType']}'),
-            Text('Salary: \Rp${widget.jobListing['jobSalary']}'),
-            // Add more job details here
-          ],
-        ),
-      ),
+            ),
     );
   }
 }
